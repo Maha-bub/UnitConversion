ed2eba5c06fd9fb6943ed953e4e0204b89fe6344
Your Name <you@example.com>
2026-02-27 21:12:21 +0600

    unit conversion

---
+++ kiloTomile.js
@@ -0,0 +1,19 @@
+// kilo meter to mile convert
+
+function kiloToMile(kilo){
+    const mile = kilo/0.6213711922;
+    return mile;
+}
+const kilo = 2;
+let mile =kiloToMile(kilo);
+console.log(mile);
+
+
+
+// mile to kilometer
+function kiloToMile(mile){
+    let kilo = mile*1.609344;
+    return kilo;
+}
+let kilo =kiloToMile(2);
+console.log(kilo);
